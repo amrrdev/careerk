b13bb219d3b721fcce3bf6393de3e4320ecc1e2c
Amr Mubarak <amrrdev@gmail.com>
2026-02-01 11:10:12 +0200

    Add Prisma Postgres integration and DB module

---
+++ src/infrastructure/database/database.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+import { Pool } from 'pg';
+import 'dotenv/config';
+
+@Injectable()
+export class DatabaseService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private pool: Pool;
+  constructor() {
+    // TODO: use @nestjs/config for proper environment management
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+    const adapter = new PrismaPg(pool);
+    super({ adapter, log: ['info', 'error', 'warn'] });
+    this.pool = pool;
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      await this.$queryRaw`SELECT 1`;
+      console.log('Prisma connected to Postgres');
+    } catch (error) {
+      console.error('Prisma connection error:', error);
+      throw error;
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    await this.pool.end();
+  }
+}
